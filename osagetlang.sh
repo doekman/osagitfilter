ff57d1e58d13280fbf1fdc32a5b86a29c9e8974f
Doeke Zanstra <doeke@archipunt.nl>
2018-06-27 14:24:49 +0200

    osagetlang now detects AppleScript below Sierra too. Issue #3

--- osagetlang.sh
+++ osagetlang.sh
@@ -8,16 +8,25 @@ use framework "OSAKit"
 on run args
 	if (count of args) is not 1 then error "usage: osagetlang path-to-script-file.scpt" number 1
 	set scpt_path to item 1 of args
+	if scpt_path does not start with "/" then
+		# If it's not an absolute path, make it (since 'read' below gives the following warning on stderr otherwise)
+		#    ...CFURLGetFSRef was passed an URL which has no scheme (the URL will not work with other CFURL routines)
+		set base_folder to POSIX path of ((path to me as text) & "::")
+		set scpt_path to base_folder & scpt_path
+	end
 	set source_nsurl to current application's |NSURL|'s fileURLWithPath:scpt_path
 	set the_script to current application's OSAScript's alloc()'s initWithContentsOfURL:source_nsurl |error|:(missing value)
 	if the_script is missing value then error "Script cannot be loaded by OSAScript" number 2
 	set osa_lang to the_script's language()'s |name| as text
 	if osa_lang is "AppleScript" then
-		#verify if it's really AppleScript (OSAScript thinks even a JPEG is an AppleScript file)
-		set cmd to "file --brief " & quoted form of scpt_path
-		set magic_file_type to do shell script cmd
-		if magic_file_type is not "AppleScript compiled" then
-			error "File is not an OSA script file (magic says: " & magic_file_type & ")" number 3
+		# double check if it's really AppleScript (OSAScript thinks even a JPEG is an AppleScript file) by checking the header
+		# (this was first done with the unix 'file' command, but AppleScript is only recognized by OS X Sierra and later)
+		set expected_header to "FasdUAS"
+		set bytes_to_read to length of expected_header
+		set file_handle to POSIX file scpt_path
+		set file_header to read file_handle from 1 to bytes_to_read
+		if expected_header ≠ file_header then
+			error "File is not an OSA script file (the first " & bytes_to_read & " characters are '" & file_header & "')" number 3
 		end if
 	end if
 	get osa_lang
