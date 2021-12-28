f3ffa31f6398868a7c62d355e30b63419a802fa9
Doeke Zanstra <doekman@icloud.com>
2021-12-28 21:27:56 +0100

    Fixed some bugs in setup.sh
    setup would fail on Big Sur (shift from 10.x naming to 11+)

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+# Unofficial Bash Strict Mode
+set -euo pipefail
+IFS=$'\n\t'
 
 function abspath {
 	if [[ -d "$1" ]]; then
@@ -17,31 +20,35 @@ function abspath {
 
 function root_check {
 	if [[ $EUID -ne 0 ]]; then
-		>&2 echo "Please run script with 'sudo $(basename $0) $@', exiting..."
+		>&2 echo "Please run script with 'sudo $(basename "$0") $*', exiting..."
 		exit 1
 	fi
 }
 
 function os_check {
 	VERSION=$(sw_vers | grep "ProductVersion" | sed -E 's/[^.0-9]+//g')
-	#From "10.11.12" get "11"
-	MAJOR=${VERSION:3:2}
+	#From "10.11.12" (sw_vers) get "11"
+	#From "12.0.1" (sw_vers) get "12"
+	MAJOR=$(echo "$VERSION" | awk -F . '{ if ($1 >= 11) print $1; else print $2; }')
 	MIN_MAJOR_VER=10
 	MIN_OS_NAME=Yosemite
 	if [[ $MAJOR -lt $MIN_MAJOR_VER ]]; then
 		>&2 echo "osagitfilter needs at least macOS 10.$MIN_MAJOR_VER ($MIN_OS_NAME) but you have macOS ${VERSION}, exiting..."
 		exit 1
 	elif [[ -n $1 ]]; then
-		echo "osagitfilter needs at least macOS 10.$MIN_MAJOR_VER ($MIN_OS_NAME) but you have macOS ${VERSION}, so that's OK..."
+		if [[ $VERBOSE == 1 ]]; then
+			echo "osagitfilter needs at least macOS 10.$MIN_MAJOR_VER.* ($MIN_OS_NAME) but you have macOS ${VERSION}, so that's OK..."
+		fi
 	fi
 }
 
 SCRIPT_NAME=$0
-BASE_DIR=$(dirname $(abspath $0))
+BASE_DIR=$(dirname "$(abspath "$0")")
 INSTALL_INTO=/usr/local/bin
-COMMANDS='osagetlang osagitfilter'
+COMMANDS=$'osagetlang\tosagitfilter'
 LOG_PATH=~/Library/Logs/Catsdeep/
 VERBOSE=0
+GIT_OPTION=""
 C=unknown
 
 while [[ $# -gt 0 ]]; do
@@ -65,9 +72,9 @@ if [[ $C = configure ]]; then
 	fi
 	for CMD in $COMMANDS; do
 		if [[ -f $INSTALL_INTO/$CMD ]]; then
-			echo "- ERROR: the command '$CMD' is already exists in '$INSTALL_INTO'. No install."
+			echo "- WARNING: the command '$CMD' is already exists in '$INSTALL_INTO'."
 		else
-			if ln -s $BASE_DIR/$CMD.sh $INSTALL_INTO/$CMD; then
+			if ln -s "$BASE_DIR/$CMD.sh" "$INSTALL_INTO/$CMD"; then
 				echo "- '$CMD' installed"
 			else
 				echo "- ERROR: couldn't create symbolic link ($?)"
@@ -94,20 +101,20 @@ if [[ $C = configure ]]; then
 elif [[ $C = reset ]]; then
 	for CMD in $COMMANDS; do
 		if [[ -h $INSTALL_INTO/$CMD ]]; then
-			if rm -f $INSTALL_INTO/$CMD; then
+			if rm -f "$INSTALL_INTO/$CMD"; then
 				echo "- '$CMD' reset"
 			else
 				echo "- ERROR: couldn't remove '$CMD' ($?)"
 			fi
 		else
-			echo "- ERROR: the command '$CMD' is not installed in '$INSTALL_INTO' as symbolic link."
+			echo "- WARNING: the command '$CMD' is not installed in '$INSTALL_INTO' as symbolic link."
 		fi
 	done
 	if [[ $(which git) ]]; then
 		echo "Removing git configuration"
-		git config --global --remove-section filter.osa
+		git config --global --remove-section filter.osa || echo "- WARNING: git exitted with code $?"
 	else
-		echo "git is not found; filter is not removed."
+		echo "WARNING: git is not found; filter is not removed."
 	fi
 elif [[ $C = rotate ]]; then
 	LOG_NAME=osagitfilter
@@ -116,7 +123,7 @@ elif [[ $C = rotate ]]; then
 		echo "Rotating log (if any) in $LOG_PATH:"
 		if [[ -f $LOG_PATH/$LOG_NAME.log ]]; then
 			echo "- renaming $LOG_NAME.log -> ${ROTATE_STAMP}_$LOG_NAME.log"
-			mv $LOG_PATH/$LOG_NAME.log $LOG_PATH/${ROTATE_STAMP}_$LOG_NAME.log
+			mv $LOG_PATH/$LOG_NAME.log "$LOG_PATH/${ROTATE_STAMP}_$LOG_NAME.log"
 		fi
 	else
 		echo "Creating log directory, nothing to rotate"
@@ -124,14 +131,14 @@ elif [[ $C = rotate ]]; then
 	fi
 	touch $LOG_PATH/$LOG_NAME.log
 elif [[ $C = create_local_bin ]]; then
-	root_check
+	root_check "$@"
 	if [[ ! -d $INSTALL_INTO ]]; then
 		echo "Creating $INSTALL_INTO"
 		mkdir $INSTALL_INTO
 	fi
-	if [[ $(stat -f '%u' $INSTALL_INTO) != $(id -u ${SUDO_USER}) ]]; then
+	if [[ $(stat -f '%u' $INSTALL_INTO) != $(id -u "${SUDO_USER}") ]]; then
 		echo "Change ownership to ${SUDO_USER}:admin"
-		chown -R ${SUDO_USER}:admin $INSTALL_INTO
+		chown -R "${SUDO_USER}:admin" $INSTALL_INTO
 	fi
 	if [[ ":$PATH:" == *":$INSTALL_INTO:"* ]]; then
 		echo "The folder $INSTALL_INTO is created successfully."
